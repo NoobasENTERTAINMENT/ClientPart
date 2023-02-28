8b018451b99007a9b35f1e14b60df4ca8628f81d
Dmitriy Ponomarev <pnomarevd38@mail.ru>
2023-02-28 13:33:25 +0300

    Добавьте файлы проекта.

---
+++ homework/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using homework.ViewModel;
+
+namespace homework
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public DisplayRootRegistry displayRootRegistry = new DisplayRootRegistry();
+        GlobalViewModel globalViewModel;
+
+        public App()
+        {
+            displayRootRegistry.RegisterWindowType<GlobalViewModel, MainWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            globalViewModel = new GlobalViewModel();
+
+            await displayRootRegistry.ShowModalPresentation(globalViewModel);
+
+            Shutdown();
+        }
+    }
+}
---
+++ homework/ViewModel/GlobalViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Windows.Input;
+
+namespace homework.ViewModel
+{
+    public class GlobalViewModel : ViewModelBase
+    {
+        public GlobalViewModel()
+        {
+
+        }
+    }
+}
